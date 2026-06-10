533de2a20aaac07ed82d58174f962ff6f0d3b48b
Alexander <121314113+coffecoderboy@users.noreply.github.com>
2026-06-11 01:02:23 +0200

    Bankkonto Klasse erstellt

---
+++ Bankkonto.java
@@ -0,0 +1,34 @@
+public class Bankkonto {
+
+    private double kontostand;
+
+    // Konstruktor: Erstellt ein neues Konto mit einem Startguthaben
+    public Bankkonto(double startguthaben) {
+        if (startguthaben < 0) {
+            throw new IllegalArgumentException("Startguthaben darf nicht negaiv sein.");      
+        }
+        this.kontostand = startguthaben;
+    }
+
+    public double getKontostand() {
+        return kontostand;
+    }
+
+    public void einzahlen(double betrag) {
+        if (betrag <= 0) {
+            throw new IllegalArgumentException("Betrag muss größer als 0 sein.");
+        }
+        this.kontostand += betrag;
+    }
+
+    public void abheben(double betrag) {
+        if (betrag <= 0) {
+            throw new IllegalArgumentException("Betrag muss größer als 0 sein.");
+        }
+        // Hier provozieren wir die Exception für deinen Test!
+        if (betrag > this.kontostand) {
+            throw new IllegalArgumentException("Nicht genügend Guthaben.");
+        }
+        this.kontostand -= betrag;
+    }
+}
