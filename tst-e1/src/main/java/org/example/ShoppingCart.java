5687006d0aa1bfd65ec45c73cf8b68d769a628c9
Alexander <121314113+coffecoderboy@users.noreply.github.com>
2026-06-11 21:34:41 +0200

    empty cart test pass (GREEN)

--- tst-e1/src/main/java/org/example/ShoppingCart.java
+++ tst-e1/src/main/java/org/example/ShoppingCart.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class ShoppingCart {
-    
+
     public int getTotal() {
         // TDD RED-Phase: Wir geben absichtlich etwas Falsches zurück
-        return -1; 
+        return 0; 
     }
 }
