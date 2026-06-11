4a9adb7de01b63063477fefe1f3eb7bba375aff8
Alexander <121314113+coffecoderboy@users.noreply.github.com>
2026-06-11 22:24:46 +0200

    add item list and for loop

--- tst-e1/src/main/java/org/example/ShoppingCart.java
+++ tst-e1/src/main/java/org/example/ShoppingCart.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
 
+    
+    private List<Item> items = new ArrayList<>();
+
     public int getTotal() {
-        // TDD RED-Phase: Wir geben absichtlich etwas Falsches zurück
-        return 0; 
+        
+        int total = 0;
+        for (Item item : items) {
+            total += item.price(); 
+        }
+        return total;
     }
+    
 
     public void addItem(Item item) {
-        // TDD RED-Phase: Wir tun so, als ob wir den Artikel hinzufügen, aber wir speichern ihn nicht wirklich
+        items.add(item);
     }
+
+    
 }
